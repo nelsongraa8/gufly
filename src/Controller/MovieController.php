5817125e10e5622a6436808aee68751fe1856f21
Nelson R. Graña <nelsongraa8@gmail.com>
2021-09-21 01:33:09 -0400

    Funcionando el allmoviedata para que devulva todas las peliculas y su informacion de la DB

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,7 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Movies;
@@ -14,7 +15,7 @@ class MovieController extends AbstractController
     /**
      * @Route("/moviedata/{id}", name="movie")
      */
-    public function index( int $id ): Response
+    public function movie( int $id ): JsonResponse
     {
 
         $movies = $this->getDoctrine()->getRepository(Movies::class)->find($id);
@@ -38,6 +39,47 @@ class MovieController extends AbstractController
             'director' => $movies->getDirector(),
             'genero' => $movies->getGenero(),
         ]);
+
+    }
+
+    /**
+     * @Route("/allmoviedata", name="allmovie")
+     */
+    public function allmovie( Request $request, MoviesRepository $moviesrepository )
+    {
+
+        $movies = $moviesrepository->findAll();
+
+        $moviesAsArray = [];
+        foreach ($movies as $movie) {
+            $moviesAsArray[] = [
+                'nombre' => $movie->getNombre(),
+                'anno' => $movie->getAnno(),
+                'productora' => $movie->getProductora(),
+                'descripcion' => $movie->getDescripcion(),
+                'poster' => $movie->getPoster(),
+                'fanart' => $movie->getFanart(),
+                'url' => $movie->getUrl(),
+                'idioma_subtitulo' => $movie->getIdiomaSubtitulo(),
+                'duracion' => $movie->getDuracion(),
+                'director' => $movie->getDirector(),
+                'genero' => $movie->getGenero(),
+            ];
+        }
+
+        if( !$movies ) {
+            return $this->json([
+                'message' => 'No product found for id ',
+            ]);
+        }
+
+        $response = new JsonResponse;
+        $response->setData([
+            'success' => true,
+            'data' => $moviesAsArray
+        ]);
+
+        return $response;
 
     }
 }
