92ada6e6c688e04330b920c101dd6045e3e3c8ee
Nelson R. Graña <nelsongraa8@gmail.com>
2021-09-28 21:41:56 -0400

    creacion de un construct y de metodos para mejorar la depuracion del codigo en el controlador MoviesController

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -20,7 +20,7 @@ class MovieController extends AbstractController
     {
 
         return $this->json([
-            'message' => 'use directamente /allmoviedata',
+            'message' => 'Use directamente /allmoviedata',
         ]);
 
     }
@@ -36,12 +36,70 @@ class MovieController extends AbstractController
         $movies = $moviesrepository->findAll();
 
         /** Verificar si se devolvio algun elemento */
-        if( !$movies ) {
-            return $this->json([
-                'message' => 'Lo sentimos! No hay peliculas',
-            ]);
-        }
+        if( !$movies ) { return $this->verification_em( $movies ); }
 
+        /** Retornar el response hecho de JSON */
+        return $this->array_em_json( $movies );
+
+    }
+
+
+    /**
+     * @Route("/moviedata/{id}", name="movie")
+     */
+    public function movie( int $id ): JsonResponse
+    {
+
+        /** Traigo el repository en el que voy a trabajar como un parametro del metodo */
+        $movies = $this->getDoctrine()->getRepository(Movies::class)->findBy(['id'=>$id]);
+
+        /** Verificar si se devolvio algun elemento */
+        if( !$movies ) { return $this->verification_em( $movies ); }
+
+        /** Retornar el response hecho de JSON */
+        return $this->array_em_json( $movies );
+
+    }
+
+
+    /**
+     * @Route("/relevantesdata", name="relevantes")
+     */
+    public function relevantes(): JsonResponse
+    {
+
+        /** Traigo el repository en el que voy a trabajar como un parametro del metodo */
+        $em = $this->getDoctrine()->getManager();
+        $relevantes = $em->getRepository(Movies::class)->findBy(['relevante' => true ]);
+
+        /** Verificar si se devolvio algun elemento */
+        if( !$relevantes ) { return $this->verification_em( $relevantes ); }
+
+        /** Retornar el response hecho de JSON */
+        return $this->array_em_json( $relevantes );
+
+    }
+
+    /** Permite que el server haga 200 OK a un cliente diferente de este host */
+    function __construct()
+    {
+        header('Access-Control-Allow-Origin:'.$_ENV['CLIENT_URL']);
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    }
+
+    /** Simple verificacion y mensaje de respuesta en formato JSON si no hay resultadso en la DB */
+    public function verification_em( $var_verify )
+    {
+        return $this->json([
+            'message' => 'Lo sentimos! No hay peliculas',
+        ]);
+    }
+
+    /** Formateo de la salida de un array en JSON, en llamado desde cada metodo de este controlador */
+    public function array_em_json( $movies )
+    {
         /** movies hay que transformarlo en un array para despues mostrarlo en un JSON */
         $moviesAsArray = [];
         foreach ($movies as $movie) {
@@ -62,101 +120,11 @@ class MovieController extends AbstractController
 
         /** Devolver los datos como JSON y mandar en el el array que se creo con el foreach() */
         $response = new JsonResponse;
-        $response->setData([
+
+        return $response->setData([
             'success' => true,
             'data' => $moviesAsArray
         ]);
-
-        header('Access-Control-Allow-Origin: http://localhost:4200');
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-
-        /** Retornar el response hecho de JSON */
-        return $response;
-
-    }
-
-
-    /**
-     * @Route("/moviedata/{id}", name="movie")
-     */
-    public function movie( int $id ): JsonResponse
-    {
-
-        $movies = $this->getDoctrine()->getRepository(Movies::class)->find($id);
-
-        if( !$movies ) {
-            return $this->json([
-                'message' => 'No product found for id '.$id,
-            ]);
-        }
-
-        return $this->json([
-            'nombre' => $movies->getNombre(),
-            'anno' => $movies->getAnno(),
-            'productora' => $movies->getProductora(),
-            'descripcion' => $movies->getDescripcion(),
-            'poster' => $movies->getPoster(),
-            'fanart' => $movies->getFanart(),
-            'url' => $movies->getUrl(),
-            'idioma_subtitulo' => $movies->getIdiomaSubtitulo(),
-            'duracion' => $movies->getDuracion(),
-            'director' => $movies->getDirector(),
-            'genero' => $movies->getGenero(),
-        ]);
-
-    }
-
-
-    /**
-     * @Route("/relevantesdata", name="relevantes")
-     */
-    public function relevantes(): JsonResponse
-    {
-
-        $em = $this->getDoctrine()->getManager();
-        $relevantes = $em->getRepository(Movies::class)->findBy(['relevante' => true ]);
-
-        if( !$relevantes ) {
-            return $this->json([
-                'message' => 'Lo sentimos! No hay peliculas relevenates',
-            ]);
-        }
-
-        /** movies hay que transformarlo en un array para despues mostrarlo en un JSON */
-        $moviesAsArray = [];
-        foreach ($relevantes as $movie) {
-            $moviesAsArray[] = [
-                'nombre' => $movie->getNombre(),
-                'anno' => $movie->getAnno(),
-                'productora' => $movie->getProductora(),
-                'descripcion' => $movie->getDescripcion(),
-                'poster' => $movie->getPoster(),
-                'fanart' => $movie->getFanart(),
-                'url' => $movie->getUrl(),
-                'idioma_subtitulo' => $movie->getIdiomaSubtitulo(),
-                'duracion' => $movie->getDuracion(),
-                'director' => $movie->getDirector(),
-                'genero' => $movie->getGenero(),
-            ];
-        }
-
-        /** Devolver los datos como JSON y mandar en el el array que se creo con el foreach() */
-        $response = new JsonResponse;
-        $response->setData([
-            'success' => true,
-            'data' => $moviesAsArray
-        ]);
-
-        header('Access-Control-Allow-Origin:'.$_ENV['CLIENT_URL']);
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-
-        /** Retornar el response hecho de JSON */
-        return $response;
-
     }
 
 }
