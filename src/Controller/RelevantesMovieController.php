108179642e3b7a6e1d852f1d825908d840e53fb0
Nelson R. Graña <87838742+nelsongraa8@users.noreply.github.com>
2022-01-06 17:01:18 -0500

    Errores corregidos en el controlador relevantesmovie

--- src/Controller/RelevantesMovieController.php
+++ src/Controller/RelevantesMovieController.php
@@ -2,29 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\MoviesRepository;
+use App\Service\SalidaDataMovieService;
+use App\Service\VerificationMovieDBService;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RelevantesMovieController extends AbstractController
 {
+    public $moviesRepository;
+
+    /** Permite que el server haga 200 OK a un cliente diferente de este host */
+    function __construct(MoviesRepository $repos)
+    {
+        $this->moviesRepository = $repos;
+
+        header('Access-Control-Allow-Origin:' . $_ENV['CLIENT_URL']);
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    }
+
     /**
      * @Route("/relevantesdata", name="relevantes")
      */
     public function relevantes(): JsonResponse
     {
-
         /** Traigo el repository en el que voy a trabajar como un parametro del metodo */
-        $em = $this->getDoctrine()->getManager();
-        $relevantes = $em->getRepository(Movies::class)->findBy(['relevante' => true]);
+        $movies = $this->moviesRepository
+            ->findBy(['relevante' => true]);
 
         /** Verificar si se devolvio algun elemento */
-        // if (!$relevantes) {
-        //     return $this->verification_em($relevantes);
-        // }
+        if (!$movies) {
+            $verificationMovieDBService = new VerificationMovieDBService;
+            return $verificationMovieDBService->VerificationEM($movies);
+        }
 
-        /** Retornar el response hecho de JSON */
-        $response = new JsonResponse;
-        return $response->setData($this->array_em_json($relevantes));
+        /** Devolver los datos como JSON y mandar en el el array que se creo con el foreach() */
+        $formatsalida = new SalidaDataMovieService;
+        $responsejson = new JsonResponse;
+        return $responsejson->setData(
+            $formatsalida->FormatSalidaMovieArrayJSON($movies)
+        );
     }
 }
